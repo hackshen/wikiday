9336cc2b70eb24e3d3fbe2b9bea8fc343e5b6ddc
Hshen <hackshen.com@gmail.com>
2024-01-07 19:23:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const https = require('https');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const URL = 'https://en.wikipedia.org/wiki/Wikipedia:Picture_of_the_day';
-
+// const URL = 'https://en.wikipedia.org/wiki/Wikipedia:Picture_of_the_day';
+const URL = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1';
 https.get(URL, function (res) {
     let chunks = [],
         size = 0;
@@ -14,8 +14,11 @@ https.get(URL, function (res) {
         const data = Buffer.concat(chunks, size);
         const html = data.toString();
         const $ = cheerio.load(html);
-        const imgUrl = $('.mw-file-description').find('img')[0].src;
-         console.log(imgUrl)
+        // const imgUrl = $('.mw-file-description').find('img')[0].src;
+        const { images } = JSON.parse(html || null);
+        const [{ url }={}] = images;
+        const imgUrl = `https://www.bing.com${url}`;
+        console.log(imgUrl);
         fs.writeFile('./README.md', `![Le Juif polonais](https:${imgUrl})`, e => {
 
         });
